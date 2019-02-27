2d30ff548c7db10025cb6ea99b6a75f14c9855ef
Deonte Cooper <deonte.cooper12@gmail.com>
2019-02-26 22:28:14 -0700

    Added encrypt letter shift method

--- lib/encryption.rb
+++ lib/encryption.rb
@@ -3,9 +3,17 @@ class Encryption
     ("a".."z").to_a << " "
   end
 
-  def encrypt_letter_index
-    letter = " "
-    number = 34
+  def encrypt_letter_shift(letter,number)
+    new = ""
+    if characters.index(letter).nil?
+      new << letter
+    else
+      new << characters.rotate(encrypt_letter_index(letter,number)).first
+    end
+    new
+  end
+
+  def encrypt_letter_index(letter,number)
    characters.index(letter) + number
   end
 end
