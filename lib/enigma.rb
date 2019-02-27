96a58bd0fe56bffd7788132920e8f221eb86ab90
Deonte Cooper <deonte.cooper12@gmail.com>
2019-02-27 01:19:02 -0700

    Added method for decrypt

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -17,4 +17,19 @@ class Enigma
     @date = date
     @output = Hash.new
   end
+
+  def encrypt(message = @message , key = @key , date = @date)
+    @output[:encryption] = encrypt_message(message)
+    @output[:key] = key
+    @output[:date] = date
+    @output
+  end
+
+  def decrypt(encrypted_message, key = @key, date = @date)
+    {
+      decryption: decrypt_message(encrypted_message),
+      key: key,
+      date: date
+    }
+  end
 end
