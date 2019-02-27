23caaf063ca268fa50eaca1ba2862ad4b5344432
Deonte Cooper <deonte.cooper12@gmail.com>
2019-02-27 14:08:14 -0700

    Added arguments to encrypt and deccrypt

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -4,12 +4,12 @@ handle = File.open(ARGV[0], "r")
 incoming_text = handle.read
 
 handle.close
-enigma = Enigma.new(incoming_text, "97419",)
-decrypted_text = enigma.decrypt(incoming_text, "97419")
+enigma = Enigma.new
+decrypted_text = enigma.decrypt(incoming_text, "91645")
 
 writer = File.open(ARGV[1], "w")
 
-writer.write(decrypted_text)
+writer.write(decrypted_text[:decryption])
 
 writer.close
-puts "Created 'decryption.txt' with a key #{enigma.key} and date #{enigma.date}"
+puts "Created #{ARGV[1]} with a key #{enigma.key} and date #{enigma.date}"
