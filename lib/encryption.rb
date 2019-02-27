d31feb1a9dd5b62210b7ce8c563e8c507452ca82
Deonte Cooper <deonte.cooper12@gmail.com>
2019-02-26 22:18:04 -0700

    Added encrypt letter index method

--- lib/encryption.rb
+++ lib/encryption.rb
@@ -2,4 +2,10 @@ class Encryption
   def characters
     ("a".."z").to_a << " "
   end
+
+  def encrypt_letter_index
+    letter = " "
+    number = 34
+   characters.index(letter) + number
+  end
 end
