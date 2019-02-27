498fd34138082af25eee5077ad58b87b4e118932
Deonte Cooper <deonte.cooper12@gmail.com>
2019-02-26 21:36:46 -0700

    Added test for number generator

--- test/generate_num_test.rb
+++ test/generate_num_test.rb
@@ -1,1 +1,14 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/generate_num'
+require './lib/enigma'
+require 'pry'
 
+class GenerateNumberTest < Minitest::Test
+
+  def test_if_it_a_generate_a_number
+    generate_num = GenerateNumber.new
+    assert_instance_of String, generate_num.random_num_generator
+    assert_equal 5, generate_num.random_num_generator.length
+  end
+end
