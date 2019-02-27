421f3099d342a7f8e00110161937d239bac86b5f
Deonte Cooper <deonte.cooper12@gmail.com>
2019-02-27 00:32:51 -0700

    Added method for letter shift

--- lib/decryption.rb
+++ lib/decryption.rb
@@ -2,6 +2,14 @@ class Decryption
   def characters
     ("a".."z").to_a << " "
   end
+  
+  def decrypt_letter_shift(letter, number)
+    if characters.index(letter).nil?
+      letter
+    else
+      characters.rotate(decrypt_letter_index(letter,number)).first
+    end
+  end
 
   def decrypt_letter_index(letter,number)
     characters.index(letter) - number
