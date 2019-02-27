a0894c41818f78f2bb8a5e362adc09705a5e0a17
Deonte Cooper <deonte.cooper12@gmail.com>
2019-02-27 01:01:13 -0700

    Change key class to module and refactored

--- lib/key.rb
+++ lib/key.rb
@@ -1,11 +1,11 @@
-class Key
+module Key
+
   def keys
-    generated_number = "02715"
     keys = {}
-    keys[:A_key] = generated_number[0..1].to_i
-    keys[:B_key] = generated_number[1..2].to_i
-    keys[:C_key] = generated_number[2..3].to_i
-    keys[:D_key] = generated_number[3..4].to_i
+    keys[:A_key] = @key[0..1].to_i
+    keys[:B_key] = @key[1..2].to_i
+    keys[:C_key] = @key[2..3].to_i
+    keys[:D_key] = @key[3..4].to_i
     keys
   end
 
@@ -19,8 +19,7 @@ class Key
   end
 
   def date_squared
-    date = "040895"
-    new_date = date.to_i ** 2
+    new_date = @date.to_i ** 2
     new_date.to_s
   end
 
