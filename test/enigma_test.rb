1174f17f039a59d043f5e18dcc3b730685525498
Deonte Cooper <deonte.cooper12@gmail.com>
2019-02-27 01:15:45 -0700

    Added test for encrypt method

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -15,4 +15,14 @@ class EnigmaTest < Minitest::Test
     assert_instance_of Enigma, enigma
   end
 
+  def test_it_can_ecrypt_a_messsage_and_return_a_hash
+    enigma = Enigma.new("hello world", "02715", "040895")
+    expected = {
+    encryption: "keder ohulw",
+    key: "02715",
+    date: "040895"
+  }
+    assert_equal expected, enigma.encrypt("hello world", "02715", "040895")
+  end
+
 end
